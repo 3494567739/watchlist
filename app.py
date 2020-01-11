11f8a524c6d98ae50856e09e8a54873c2fcf21a6
NicolasXiong <3494775639@qq.com>
2020-01-11 21:03:32 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -8,9 +8,57 @@ Created on Sat Jan 11 07:37:07 2020
 from flask import Flask
 from flask import url_for
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
+import os
+import click
 
-name = 'Nicolas Xiong'
-movies = [{'title': 'My Neighbor Totoro', 'year': '1988'},
+
+
+app=Flask(__name__) 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
+
+# @app.route('/') 
+# @app.route('/index') 
+# @app.route('/home')
+# def hello():   
+#     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+# @app.route('/static/favicon.ico')
+# def get_fav():
+#     print(__name__)
+#     return app.send_static_file('favicon.ico')
+
+# @app.route('/user/<name>') 
+# def user_page(name):    
+#     return 'User: %s' % name
+
+# @app.route('/test') 
+# def test_url_for():    
+#     # 下面是一些调用示例：    
+#     print(url_for('index'))  # 输出：/    
+#     # 注意下面两个调用是如何生成包含 URL 变量的 URL 的    
+#     print(url_for('user_page', name='nicolasxiong'))  # 输出：/user/nicolasxoing    
+#     print(url_for('test_url_for'))  # 输出：/test    
+#     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。    
+#     print(url_for('test_url_for', num=2))  # 输出：/test?num=2    
+#     return 'Test page'
+
+@app.route('/') 
+def index():    
+    user = User.query.first()  # 读取用户记录    
+    movies = Movie.query.all()  # 读取所有电影记录
+    return render_template('index.html',user=user, movies=movies)
+
+@app.cli.command() 
+def forge():    
+    """Generate fake data."""    
+    db.create_all()
+    
+    name = 'Nicolas Xiong'
+    movies = [
+                           {'title': 'My Neighbor Totoro', 'year': '1988'},
                            {'title': 'Dead Poets Society', 'year': '1989'},    
                            {'title': 'A Perfect World', 'year': '1993'},    
                            {'title': 'Leon', 'year': '1994'},    
@@ -19,38 +67,29 @@ movies = [{'title': 'My Neighbor Totoro', 'year': '1988'},
                            {'title': 'King of Comedy', 'year': '1999'},    
                            {'title': 'Devils on the Doorstep', 'year': '1999'},    
                            {'title': 'WALL-E', 'year': '2008'},
-                           {'title': 'The Pork of Music', 'year': '2012'}]
+                           {'title': 'The Pork of Music', 'year': '2012'}
+                           ]
+    
+    user = User(name=name)    
+    db.session.add(user)    
+    for m in movies:        
+         movie = Movie(title=m['title'], year=m['year'])        
+         db.session.add(movie)
+         
+    db.session.commit()    
+    click.echo('Done.')
 
-app=Flask(__name__) 
-# @app.route('/') 
-# @app.route('/index') 
-# @app.route('/home')
-# def hello():   
-#     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 
-@app.route('/') 
-def index():    
-    return render_template('index.html', name=name, movies=movies)
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）    
+    id = db.Column(db.Integer, primary_key=True)  # 主键    
+    name = db.Column(db.String(20))  # 名字
+    
+class Movie(db.Model):  # 表名将会是 movie    
+    id = db.Column(db.Integer, primary_key=True)  # 主键    
+    title = db.Column(db.String(60))  # 电影标题    
+    year = db.Column(db.String(4))  # 电影年份
 
-@app.route('/user/<name>') 
-def user_page(name):    
-    return 'User: %s' % name
 
-@app.route('/test') 
-def test_url_for():    
-    # 下面是一些调用示例：    
-    print(url_for('index'))  # 输出：/    
-    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的    
-    print(url_for('user_page', name='nicolasxiong'))  # 输出：/user/nicolasxoing    
-    print(url_for('test_url_for'))  # 输出：/test    
-    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。    
-    print(url_for('test_url_for', num=2))  # 输出：/test?num=2    
-    return 'Test page'
-
-# @app.route('/static/favicon.ico')
-# def get_fav():
-#     print(__name__)
-#     return app.send_static_file('favicon.ico')
 
 
 
